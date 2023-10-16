ba923a9a40b8987229ba650595c52561d9c35001
EduardoFurio <eduardofurio22@gmail.com>
2023-10-16 16:45:25 +0200

    Terzo commit da eduardoBranch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ciao, prima prova Git!!");
         System.out.println("sono eduardobranch");
+        System.out.println("secondo push con eduardobranch");
+        System.out.println("secondo push con eduardobranch");
+
 
     }
 }
