4afe2cb974bd54c391fedfc08069c3a400732620
EduardoFurio <eduardofurio22@gmail.com>
2023-10-16 16:20:37 +0200

    cambio da eduardoBranch Chill

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("ciao, prima prova Git!!");
-        System.out.println("Secondo commit!");
+        System.out.println("sono eduardobranch");
 
     }
 }
