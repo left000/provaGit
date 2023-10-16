d399131187f48814a65d7717bc8f7b5e26c72bd2
EduardoFurio <eduardofurio22@gmail.com>
2023-10-16 16:12:55 +0200

    secondo commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("ciao, prima prova Git!!");
+        System.out.println("Secondo commit!");
+
     }
 }
